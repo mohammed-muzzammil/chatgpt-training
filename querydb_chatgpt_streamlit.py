c435f44d7f862ea4cd1ef35e2193099e1bcef12b
Mohammed Muzzammil <36696171+mohammed-muzzammil@users.noreply.github.com>
2023-07-04 11:55:09 +0530

    Add files via upload

---
+++ querydb_chatgpt_streamlit.py
@@ -0,0 +1,225 @@
+import openai  # Importing the OpenAI library
+import streamlit as st  # Importing the Streamlit library for building web applications
+import pandas as pd  # Importing the pandas library for data manipulation
+from sqlalchemy import create_engine, inspect, text  # Importing SQLAlchemy for database operations
+
+openai.api_key = "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"  # Setting the OpenAI API key
+
+# Create a connection to the database
+db_host = 'localhost'
+db_port = 3306
+db_user = 'root'
+db_password = 'mysecretpassword'
+db_name = 'dvd_rental'
+
+# Adding a title to the app
+st.title('DVD Rental Company')
+
+# Create a connection to the database
+# Try to create a database engine to connect to the database
+try:
+    engine = create_engine(f'mysql+pymysql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}')
+
+except Exception as e:
+    st.error(f'Could not connect to the database. Error: {e}')
+    st.stop()
+
+inspector = inspect(engine)
+
+# Get the table names
+table_names = inspector.get_table_names()
+
+# Get the column names
+column_names = {}
+
+for table_name in table_names:
+    columns = inspector.get_columns(table_name)
+    column_names[table_name] = [column['name'] for column in columns]
+
+# form an introspection doc
+doc = f"""
+        AUTO-GENERATED DATABASE SCHEMA DOCUMENTATION\n
+        Total Tables: {len(table_names)}
+        """
+for table_name in table_names:
+    doc += f"""
+    ---
+    Table Name: {table_name}\n
+    Columns:
+    """
+    for column_name in column_names[table_name]:
+        doc += f"""
+        - {column_name}
+        """
+
+
+# Function to query the database using GPT-3 and return the results
+def query_db(query):
+    # Create a prompt to query the database using GPT-3
+    intro = """
+    You are a data analyst working for a DVD rental company. You have been asked to write SQL queries to answer \
+    some business questions. You have been given a database schema and a list of questions. \
+    You need to write SQL queries to answer these questions."""
+    prompt = 'Given the database schema, write a SQL query that returns the following information: '
+    prompt += query
+    prompt += f'You only need to write SQL code, do not comment or explain code and do not add any additional info. \
+    I need code only. Always use table name in column reference to avoid ambiguity. \
+    SQL dialect is MySQL.\
+    Only use columns and tables mentioned in the doc below. \n{doc}'
+
+    # Query the database using GPT-3
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": intro},
+            {"role": "user", "content": prompt}
+        ]
+    )
+
+    # Extract the code from the response
+    code = response.choices[0].message.content
+
+    # Format the code
+    pretty_code = '```sql\n' + code + '\n```'
+
+    # Remove newlines from the code
+    code = code.replace('\n', ' ')
+
+    # Execute the query
+    print("Executing the following query:" + pretty_code)
+
+    with engine.connect() as con:
+        df = pd.read_sql_query(sql=text(code), con=engine)
+
+    # Return the results
+    return df, pretty_code
+
+
+# Function to query the database as a customer using GPT-3 and return the results
+def query_db_customer(query, customer_id):
+    # Create a prompt that allows customer to query the database using GPT-3
+    intro = """
+    You are a chatbot for a DVD rental company. You have been asked to answer some questions from customers. \
+    Customer ID is provided to you. Customer can ask you questions in natural language. \
+    You need to convert the natural language question into SQL query and execute it.
+    You will be given a database schema and based on that you need to generate SQL queries. \
+    Remember that as a customer is asking you, they will be expecting the result from thier perspective. \
+    So you need to filter the results based on the customer ID. \
+    You only need to write SQL code, do not comment or explain code and do not add any additional info. \
+    I need code only. Always use table name in column reference to avoid ambiguity. \
+    SQL dialect is MySQL.\
+    Limit the results to 10 rows. \
+    Only use columns and tables mentioned in the doc below. \n{doc}'\
+    Customer: give me all the films that I have rented so far customer id is 1
+    AI: SELECT film.film_id, film.title FROM rental JOIN inventory ON rental.inventory_id = inventory.inventory_id\
+        JOIN film ON inventory.film_id = film.film_id WHERE rental.customer_id = 1;
+        """
+
+    prompt = 'Customer: ' + query + ' customer id is ' + str(customer_id)
+
+    # Query the database using GPT-3
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": intro},
+            {"role": "user", "content": prompt}
+        ]
+    )
+
+    # Extract the code from the response
+    code = response.choices[0].message.content
+
+    # Format the code
+    pretty_code = '```sql\n' + code + '\n```'
+
+    # Remove newlines from the code
+    code = code.replace('\n', ' ')
+
+    with engine.connect() as con:
+        df = pd.read_sql_query(sql=text(code), con=engine)
+
+    summary = summarize_result(df, query)
+
+    # Return the results
+    return df, summary, pretty_code
+
+
+def summarize_result(df, query):
+    # Summarize the result prompt
+    system_prompt = """"
+    You are a chatbot for a DVD rental company.\
+    A SQL output will be provided to you in rows and columns format.\
+    You need to summarize the result based on the question asked by the customer and provide the summary in natural language.\n
+    Remember that as a customer is asking you, they will be expecting the result from thier perspective.\
+    """
+
+    prompt = f"""
+    Please summarize the result in natural language. based on the question asked by the customer.\
+    Customer: {query}\
+    SQL Output: {df.to_string(index=False)}
+    """
+
+    # Query the database using GPT-3
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": prompt}
+        ]
+    )
+
+    # Extract the summary from the response
+    summary = response.choices[0].message.content
+
+    # Return the summary
+    return summary
+
+
+# Creating the sidebar section of the Streamlit app
+with st.sidebar:
+    st.write('Who are you?')
+    user_type = st.selectbox('Select your role', ['Data Analyst', 'Customer'])
+    if user_type == 'Customer':
+        customer_id = st.number_input('Enter your customer ID', min_value=1, max_value=599, value=1)
+
+    with st.form(key='my_form_to_submit'):
+        user_request = st.text_area("Let chatGPT to do SQL for you")
+        submit_button = st.form_submit_button(label='Submit')
+
+if submit_button:  # If the submit button is clicked
+    # Check if the user has entered a request
+    if not user_request:
+        st.error('Please enter a request')
+        st.stop()
+
+    # Check if the user has entered database credentials
+    if not db_host or not db_user or not db_password or not db_name or not db_port:
+        st.error('Please enter database credentials')
+        st.stop()
+
+    # Execute the query based on the user type
+    if user_type == 'Data Analyst':
+        df, pretty_code = query_db(user_request)
+
+        # Display the results
+        st.write('SQL Output')
+        st.dataframe(df)
+
+    else:
+
+        df, summary, pretty_code = query_db_customer(user_request, customer_id)
+
+        # Display the results
+        st.write('Assistant')
+        st.write(summary)
+
+        with st.expander("See SQL Output"):
+            st.dataframe(df)
+
+    # Display the executed code in an expander section
+    with st.expander("See executed code"):
+        st.write(pretty_code)
+
+    # Display the introspected database structure in an expander section
+    with st.expander("See introspected DB structure"):
+        st.write(doc)
